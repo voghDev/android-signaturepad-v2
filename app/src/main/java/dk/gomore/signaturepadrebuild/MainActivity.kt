1109ac81dbcaac7fd9f6ecc38b90b191695eac28
Olmo Gallegos <vendofiatpunto@hotmail.com>
2019-11-22 13:53:00 +0100

    Import SignaturePad code

--- app/src/main/java/dk/gomore/signaturepadrebuild/MainActivity.kt
+++ app/src/main/java/dk/gomore/signaturepadrebuild/MainActivity.kt
@@ -1,12 +1,40 @@
 package dk.gomore.signaturepadrebuild
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dk.gomore.signaturepadrebuild.views.SignaturePad
+import kotlinx.android.synthetic.main.activity_examples_main.*
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_EXTERNAL_STORAGE = 1
+    val PERMISSIONS_STORAGE = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_examples_main)
+
+        signaturePad.setOnSignedListener(object : SignaturePad.OnSignedListener {
+            override fun onStartSigning() {
+            }
+
+            override fun onSigned() {
+                saveButton.isEnabled = true
+                clearButton.isEnabled = true
+            }
+
+            override fun onClear() {
+                saveButton.isEnabled = false
+                clearButton.isEnabled = false
+            }
+        })
+
+        clearButton.setOnClickListener {
+            signaturePad.clear()
+        }
+
+        saveButton.setOnClickListener {
+
+        }
     }
 }
